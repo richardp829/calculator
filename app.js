89ee4f1f4fa5a8493a2ffef28e3d626bf7254923
richardp829 <richardpulido2001@gmail.com>
2023-08-26 04:42:52 -0500

    add operation functions

---
+++ app.js
@@ -0,0 +1,8 @@
+function add (num1,num2) { return num1 + num2 };
+function subtract (num1,num2) { return num1 - num2 };
+function multiply (num1,num2) { return num1 * num2 };
+function divide (num1,num2) { return num1 / num2 };
+
+
+
+console.log(add(1,2),subtract(3,4) ,multiply(6,7) ,divide(4,5));
